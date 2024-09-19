3ca9fedbb9b3bc35d636d2ee1fd7091d9554e7c5
DaryaAniskevich <darya.aniskevich@mail.ru>
2024-09-19 18:34:34 +0300

    feat: trust block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import MainBlock from "@/components/main/MainBlock";
 
 export default function Home() {
   return (
-    <div className="pt-[13px] md:pt-[75px] sm:[37px] md:pb-[93px]">
+    <div className="pt-[13px] md:pt-[75px] pb-[37px] md:pb-[93px]">
       <MainBlock />
       <Benefits />
+
+      <section className="text-blue title2 font-bold text-center">Millions of people trust us!</section>
     </div>
   );
 }
